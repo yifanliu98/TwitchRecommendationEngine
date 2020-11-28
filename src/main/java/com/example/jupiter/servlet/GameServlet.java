3e2e22542dc391a202e784a1c4ec87601af3bce0
Yifan Liu <baozi@Yifans-MacBook-Pro.local>
2020-11-28 07:26:34 -0800

    setup environment

---
+++ src/main/java/com/example/jupiter/servlet/GameServlet.java
@@ -0,0 +1,25 @@
+package com.example.jupiter.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//localhost:8080/jupiter/game
+@WebServlet(name = "GameServlet", urlPatterns = {"/game"})
+public class GameServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().print("Hello World");
+    }
+
+//    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        response.getWriter().print("Hello World");
+//    }
+
+}
