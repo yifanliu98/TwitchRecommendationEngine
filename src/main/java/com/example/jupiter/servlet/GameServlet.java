ef3339cd128dc6d7704d6c7c2497b76436134915
yifanliu98 <yifan98liu@gmail.com>
2020-11-28 14:25:26 -0800

    java basis

--- src/main/java/com/example/jupiter/servlet/GameServlet.java
+++ src/main/java/com/example/jupiter/servlet/GameServlet.java
@@ -7,19 +7,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
 //localhost:8080/jupiter/game
 @WebServlet(name = "GameServlet", urlPatterns = {"/game"})
 public class GameServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        response.getWriter().print("Hello World");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.getWriter().print("Hello World");
     }
-
-//    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        response.getWriter().print("Hello World");
-//    }
 
 }
