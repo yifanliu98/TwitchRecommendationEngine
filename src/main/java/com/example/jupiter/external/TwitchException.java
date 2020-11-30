592965d2bf09133913f0e277bbf8a273aa2710e2
yifanliu98 <yifan98liu@gmail.com>
2020-11-29 16:30:53 -0800

    search game and top games

---
+++ src/main/java/com/example/jupiter/external/TwitchException.java
@@ -0,0 +1,8 @@
+package com.example.jupiter.external;
+
+public class TwitchException extends RuntimeException {
+    public TwitchException(String errorMessage) {
+        super(errorMessage);
+    }
+
+}
--- src/main/java/com/example/jupiter/servlet/GameServlet.java
+++ src/main/java/com/example/jupiter/servlet/GameServlet.java
@@ -1,5 +1,12 @@
 package com.example.jupiter.servlet;
 
+import com.example.jupiter.entity.Game;
+import com.example.jupiter.external.TwitchClient;
+import com.example.jupiter.external.TwitchException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONObject;
+import org.apache.commons.io.IOUtils;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,12 +18,20 @@ import java.io.IOException;
 //localhost:8080/jupiter/game
 @WebServlet(name = "GameServlet", urlPatterns = {"/game"})
 public class GameServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().print("Hello World");
-    }
-
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().print("Hello World");
+        String gameName = request.getParameter("game_name");
+        TwitchClient client = new TwitchClient();
+
+        response.setContentType("application/json;charset=UTF-8");
+        try {
+            if (gameName != null) {
+                response.getWriter().print(new ObjectMapper().writeValueAsString(client.searchGame(gameName)));
+            } else {
+                response.getWriter().print(new ObjectMapper().writeValueAsString(client.topGames(0)));
+            }
+        } catch (TwitchException e) {
+            throw new ServletException(e);
+        }
     }
 
 }
