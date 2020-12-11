2399b531a0b1ec54c223803ffe6385a596b75fa5
yifanliu98 <yifan98liu@gmail.com>
2020-12-10 16:45:32 -0800

    set/unset favorite item

--- src/main/java/com/example/jupiter/db/MySQLDBUtil.java
+++ src/main/java/com/example/jupiter/db/MySQLDBUtil.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 public class MySQLDBUtil {
-    private static final String INSTANCE = "webproject.cgaweydjy4xs.us-east-2.rds.amazonaws.com";
+    private static final String INSTANCE = "twitchgame.cgaweydjy4xs.us-east-2.rds.amazonaws.com";
     private static final String PORT_NUM = "3306";
     private static final String DB_NAME = "jupiter";
     public static String getMySQLAddress() throws IOException {
